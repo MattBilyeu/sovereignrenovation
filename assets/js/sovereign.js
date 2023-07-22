0a6fff41dd73e2f72b33450755070fd43b8a901f
Matt Bilyeu <matt.bilyeu1@gmail.com>
2023-07-22 14:47:48 -0400

    Finished Home Page

--- assets/js/sovereign.js
+++ assets/js/sovereign.js
@@ -30,3 +30,51 @@ lighting.addEventListener('click', ()=> {
     handleImageTransition('assets/images/sovereign/lighting-install.jpg');
 })
 
+const testimonials = document.querySelectorAll('.testimonial');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+let index = 0;
+
+const handleLeft = function() {
+    if (index === 0) {
+        index = testimonials.length-1;
+    } else {
+        index--;
+    }
+    setActive();
+    clearInterval(testimonialScroll);
+}
+
+left.addEventListener('click', ()=> {
+    handleLeft();
+})
+
+const handleRight = function() {
+    if (index === testimonials.length-1) {
+        index = 0;
+    } else {
+        index++;
+    }
+    setActive();
+    clearInterval(testimonialScroll);
+}
+
+right.addEventListener('click', ()=> {
+    handleRight();
+})
+
+const setActive = function() {
+    testimonials.forEach(item => {
+        item.classList.remove('active');
+    });
+    testimonials[index].classList.add('active');
+}
+
+const testimonialScroll = setInterval(()=> {
+    if (index === testimonials.length - 1) {
+        index = 0;
+    } else {
+        index++;
+    }
+    setActive();
+},2000);
